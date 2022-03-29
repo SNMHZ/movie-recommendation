e68354e92ee9c913cab4f5a04fc9661090234378
binyf <binyf3640@gmail.com>
2022-03-29 20:32:03 +0000

    feat(lightGCN): preprocess modified

--- practice/T3204/lightGCN/preprocess.py
+++ practice/T3204/lightGCN/preprocess.py
@@ -1,6 +1,16 @@
 import numpy as np
 import pandas as pd
 import os
+from sklearn.model_selection import train_test_split
+
+def random_split(df):
+    global train_df
+    global test_df
+    t_r, t_e = train_test_split(df, test_size=0.2)
+    train_df = pd.concat([train_df, t_r])
+    test_df = pd.concat([test_df, t_e])
+    
+
 path = '../../data/train'
 data = pd.read_csv(os.path.join(path,'train_ratings.csv'))
 users = data['user'].unique()
@@ -10,12 +20,9 @@ item2id = dict((sid, i) for (i, sid) in enumerate(items))
 encoded_user = [user2id[u] for u in data['user']]
 encoded_item = [item2id[i] for i in data['item']]
 full_df = pd.DataFrame(zip(encoded_user,encoded_item), columns=['user','item'])
-N = full_df.shape[0]
-t_N = int(N*0.2)
-test_idx = np.random.choice(range(N), t_N, replace=False)
-train_idx = np.setdiff1d(range(N), test_idx)
-train = full_df.iloc[train_idx]
-test = full_df.iloc[test_idx]
+train_df = pd.DataFrame({'user' : [], 'item' : []}, columns = ['user','item'])
+test_df = pd.DataFrame({'user' : [], 'item' : []}, columns = ['user','item'])
+full_df.groupby('user').apply(random_split)
 full_df.to_csv("../../data/train/light_gcn/full.csv", index=False)
-train.to_csv("../../data/train/light_gcn/train.csv", index=False)
-test.to_csv("../../data/train/light_gcn/test.csv", index=False)
+train_df.to_csv("../../data/train/light_gcn/train.csv", index=False)
+test_df.to_csv("../../data/train/light_gcn/test.csv", index=False)
